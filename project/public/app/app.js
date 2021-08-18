608c0d3ca7f3ad3cd2c32a55aafee9d81b1e1624
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-08-18 01:20:03 -0300

    development of class 04

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,11 +1,20 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { compose, debounceTime, partialize, pipe, takeUntil } from './utils/operators.js';
 
-document
-.querySelector('#myButton')
-.onclick = () => 
+const operations = compose(
+    partialize(debounceTime, 500),
+    partialize(takeUntil, 3)
+);
+
+const action = operations(() =>
     service
     .sumItems('2143')
     .then(console.log)
-    .catch(console.log);
+    .catch(console.log)
+);
+
+document
+.querySelector('#myButton')
+.onclick = action;
