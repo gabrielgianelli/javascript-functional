f70782387421bebd8df5e1d7d79ac3293d000110
Gabriel Gianelli <gabrielgianelli@gmail.com>
2021-08-25 23:48:54 -0300

    development of class 06

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,7 +1,8 @@
 import { retry, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { compose, debounceTime, partialize, pipe, takeUntil } from './utils/operators.js';
+import { compose, debounceTime, partialize, takeUntil } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = compose(
     partialize(debounceTime, 500),
@@ -9,8 +10,8 @@ const operations = compose(
 );
 
 const action = operations(() =>
-    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+    retry(3, 3000, () => timeoutPromise(1000, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
